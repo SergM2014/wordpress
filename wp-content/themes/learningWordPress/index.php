71004f74f8635753d4615097f222031a30836c82
SergM2014 <weisse011@gmail.com>
2015-12-28 11:28:42 +0200

    excerpt+ fetered image+

--- wp-content/themes/learningWordPress/index.php
+++ wp-content/themes/learningWordPress/index.php
@@ -5,7 +5,11 @@ get_header();
 if(have_posts()):
     while (have_posts()):the_post();
 ?>
-     <article class="post">
+     <article class="post <?php if(has_post_thumbnail()) { ?> has-thumbnail <?php } ?>">
+
+         <div class="post-thumbnail">
+            <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('small-thumbnail'); ?></a>
+         </div>
 
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 
@@ -29,7 +33,21 @@ if(have_posts()):
          ?>
          </p>
 
-        <?php the_content(); ?>
+
+
+
+
+         <?php if($post->post_exerpt){ ?>
+            <p>
+        <?php echo get_the_excerpt(); ?>
+         <a href="<?php the_permalink(); ?>">Read more&raquo;</a>
+         </p>
+        <?php } else {
+            the_content();
+         } ?>
+
+
+
 
       </article>
 
