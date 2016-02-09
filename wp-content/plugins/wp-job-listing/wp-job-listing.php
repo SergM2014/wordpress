28dbc17ac429cbcb2d318945ef1bd5c37f957e5d
SergM2014 <weisse011@gmail.com>
2016-02-09 11:53:43 +0200

    fully completed register_post_type

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -15,9 +15,59 @@ if(!defined('ABSPATH')){
 
 function dwwp_register_post_type(){
 
+    $singular = 'Job Listing';
+    $plural = 'Job Listings';
+    $labels = array(
+        'name'              =>$plural,
+        'singular_name'     =>$singular,
+        'add_name'          =>'Add New',
+        'add_new_item'      =>'Add New '.$singular,
+        'edit'              =>'Edit',
+        'edit_item'         =>'Edit '.$singular,
+        'new_item'          =>'New '.$singular,
+        'view'              =>'View '.$singular,
+        'view_item'         =>'View '.$singular,
+        'search_term'       =>'Search '.$plural,
+        'parent'            =>'Parent '.$singular,
+        'not_found'         =>'No '.$plural.' found',
+        'not_found_in_trash'=>'no '.$plural.' in trash'
+
+
+    );
+
     $args= array(
-       'public'=> true,
-        'label'=> 'Job Listing'
+        'labels'=>$labels,
+        'public'                =>true,
+        'publicly_queryable'    =>true,
+        'exclude_from_search'   =>false,
+        'show_in_nav_menus'     =>true,
+        'show_ui'               =>true,
+        'show_in_menu'          => true,
+        'show_in_admin_bar'     =>true,
+        'menu_position'         =>6,
+        'menu_icon'             =>'dashicons-bisinessman',
+        'can_export'            =>true,
+        'delete_with_user'      =>false,
+        'hierarchical'          =>false,
+        'has_archive'           =>true,
+        'query_var'             =>true,
+        'capability_type'       =>'page',
+        'map_meta_cap'          =>true,
+
+        'rewrite'=>array(
+          'slug'        =>'jobs',
+            'with_front'=>true,
+            'pages'     =>true,
+            'feeds'     =>false,
+
+        ),
+        'supports'=>array(
+            'title',
+            'editor',
+            'author',
+            'custom-fields',
+            'thumbnail'
+        )
     );
 
     register_post_type('job', $args);
