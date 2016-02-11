223bc302f79613c334c2d3ba11863ec23346fd12
SergM2014 <weisse011@gmail.com>
2016-02-11 10:12:00 +0200

    'location' taxonomy added

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -74,3 +74,43 @@ function dwwp_register_post_type(){
 }
 
 add_action('init', 'dwwp_register_post_type');
+
+function dwwp_register_taxonomy(){
+
+
+    $plural='Locations';
+    $singular='Location';
+
+    $labels= array(
+        'name'                          =>$plural,
+        'singular_name'                 =>$singular,
+        'search_items'                  =>'Search '.$plural,
+        'popular_items'                 =>'Popular '.$plural,
+        'all_items'                     =>'All '.$plural,
+        'parent_item'                   =>null,
+        'parent_item_colon'             =>null,
+        'edit_item'                     =>'Edit '.$singular,
+        'update_item'                   =>'Update '.$singular,
+        'add_new_item'                  => 'Add New '.$singular,
+        'new_item_name'                 =>'New '.$singular.' name',
+        'separate_items_with_commas'    =>'separate '.$plural.' with commas',
+        'add_or_remove_items'           =>'Add or removw '.$plural,
+        'choose_from_most_used'         =>'Choose from most used '.$plural,
+        'not_found'                     =>'No '.$plural.' found.',
+        'menu_name'                     =>$plural,
+    );
+
+    $args = array(
+      'hierarchical'                =>true,
+        'labels'                    =>$labels,
+        'show_ui'                   =>true,
+        'show_admin_column'         =>true,
+        'update_count_callback'     =>'_update_post_term_count',
+        'query_var'                 =>true,
+        'rewrite'                   =>array('slug'=>'location'),
+    );
+
+    register_taxonomy('location', 'job', $args);
+}
+
+add_action('init', 'dwwp_register_taxonomy');
