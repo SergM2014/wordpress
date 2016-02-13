ab37fa29b7e427c010090b9ef7072b9bb40793ba
SergM2014 <weisse011@gmail.com>
2016-02-13 11:31:48 +0200

    meta_box and custom fields created

---
+++ wp-content/plugins/wp-job-listing/wp-job-fields.php
@@ -0,0 +1,47 @@
+<?php
+
+function dwwp_add_custom_metabox(){
+
+    add_meta_box(
+        'dwwp_meta',
+        'Job Listing',
+        'dwwp_meta_callback',
+        'job',
+        'normal',
+        'core'
+    );
+}
+add_action('add_meta_boxes', 'dwwp_add_custom_metabox');
+
+function dwwp_meta_callback(){
+    ?>
+        <div class="meta-row">
+            <div class="meta-th">
+                <label for="job-id" class="dwwp-row-title">Job Id</label>
+            </div>
+            <div class="meta-td">
+                <input type="text" name="job-id" id="job-id" value="">
+            </div>
+
+        </div>
+        <div class="meta">
+            <div class="meta-th">
+                <span>Principle Duties</span>
+            </div>
+        </div>
+        <div class="meta-editor"><!-- </div> -->
+    <?php
+
+        $content = get_post_meta($post->ID, 'principle_duties', true );
+        $editor = 'principle_duties';
+        $settings =array(
+            'textarea_rows'=> 8,
+            'media_buttons'=>true,
+        );
+
+        wp_editor($content, $editor, $settings);
+
+    ?>
+    </div>
+    <?php
+}
