a23d643c697bd673b5346bd161eec5257e162409
SergM2014 <weisse011@gmail.com>
2016-02-17 11:19:14 +0200

    css add js files are added

--- wp-content/plugins/wp-job-listing/wp-job-listing.php
+++ wp-content/plugins/wp-job-listing/wp-job-listing.php
@@ -16,3 +16,15 @@ if(!defined('ABSPATH')){
 require_once(plugin_dir_path(__FILE__).'wp-job-cpt.php');
 require_once (plugin_dir_path(__FILE__).'wp-job-render-admin.php');
 require_once (plugin_dir_path(__FILE__).'wp-job-fields.php');
+
+
+function dwwp_admin_enqueue_scripts(){
+    global $pagenow, $typenow;
+
+    if(($pagenow == 'post.php' || $pagenow == 'post-new.php') && $typenow == 'job'){
+        wp_enqueue_style('dwwp-admin-css', plugins_url('css/admin-jobs.css', __FILE__));
+        wp_enqueue_script('dwwp-job-js', plugins_url('js/admin-jobs.js', __FILE__), array('jquery', 'jquery-ui-datepicker'), '20150204', true );
+    }
+}
+
+add_action('admin_enqueue_scripts', 'dwwp_admin_enqueue_scripts');
